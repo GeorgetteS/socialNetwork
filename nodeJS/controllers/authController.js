c625c978596f7cc8aa0f3782904da34499cb5ddf
GeorgetteS <103926946+GeorgetteS@users.noreply.github.com>
2023-07-24 17:24:40 +0300

    fix refresh token

--- nodeJS/controllers/authController.js
+++ nodeJS/controllers/authController.js
@@ -31,7 +31,7 @@ class AuthController {
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        domain: 'localhost',
+        domain: process.env.CLIENT_DOMAIN,
       });
 
       res.json(userData);
