b18994a9cd2e06ec1972033d1b47dfecc2410a64
GeorgetteS <103926946+GeorgetteS@users.noreply.github.com>
2023-07-26 13:30:26 +0300

    delete domain option

--- nodeJS/controllers/authController.js
+++ nodeJS/controllers/authController.js
@@ -31,7 +31,6 @@ class AuthController {
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        domain: process.env.CLIENT_DOMAIN,
       });
 
       res.json(userData);
